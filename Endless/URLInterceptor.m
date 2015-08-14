dc4a82721393168a438ba0ffbc2287d6e8aade87
joshua stein <jcs@jcs.org>
2015-08-14 16:35:34 -0500

    add license to individual files

--- Endless/URLInterceptor.m
+++ Endless/URLInterceptor.m
@@ -1,6 +1,32 @@
 /*
+ * Endless
+ * Copyright (c) 2014-2015 joshua stein <jcs@jcs.org>
+ *
  * CKHTTP portions of this file are from Onion Browser
  * Copyright (c) 2012-2014 Mike Tigas <mike@tig.as>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 #import "AppDelegate.h"
@@ -29,6 +55,11 @@ static NSString *_javascriptToInject;
 	if (!_javascriptToInject) {
 		NSString *path = [[NSBundle mainBundle] pathForResource:@"injected" ofType:@"js"];
 		_javascriptToInject = [NSString stringWithContentsOfFile:path encoding:NSUTF8StringEncoding error:nil];
+		
+		/* strip out license header */
+		NSError *error;
+		NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"/\\*.*?\\*/" options:NSRegularExpressionDotMatchesLineSeparators error:nil];
+		_javascriptToInject = [regex stringByReplacingMatchesInString:_javascriptToInject options:0 range:NSMakeRange(0, [_javascriptToInject length]) withTemplate:@""];
 	}
 	
 	return _javascriptToInject;
